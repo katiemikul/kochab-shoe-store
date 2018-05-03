a9f066bc274132ad2ef037adc89bbedbb3810993
katiemikul <katie.heding@gmail.com>
2018-05-03 11:54:55 -0500

    display shoes on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,25 @@ console.log('client.js has been loaded');
 
 var app = angular.module('ShoeApp', ['ngRoute']);
 
-app.controller('ShoeController', function() {
+app.controller('ShoeController', ['$http', function($http) {
     var self = this;
     self.message = 'Welcome to the Shoe Store';
-});
+
+    self.shoes = displayShoes();
+
+    displayShoes = {};
+
+    function displayShoes(){
+        $http({
+            method: 'GET',
+            url: '/shoe'
+        })
+        .then(function(response) {
+            console.log(response);
+            self.shoes = response.data;
+        })
+        .catch(function (error) {
+            console.log('error on /shoe GET', error);
+        });
+    }
+}])
